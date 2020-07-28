330eb97e874b74f336f935f6e399d8f8548e622c
Heitor Rabello <he1tor@hotmail.com>
2020-07-27 21:31:58 -0300

    add new function on keyBoardFunctions

--- script/main.js
+++ script/main.js
@@ -20,6 +20,9 @@ class Stopwatch {
                 } else {
                     stopwatch.start();
                 }
+            },
+            Enter() {
+                if (stopwatch.running) stopwatch.saveResult();
             }
         };
 
