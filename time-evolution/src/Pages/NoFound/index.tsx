e2c6cd14a164924d8f5fbc7290f8f1118bc70bd8
Heitor Rabello <he1tor@hotmail.com>
2020-10-27 22:18:23 -0300

    Style page 404 :construction:

--- time-evolution/src/Pages/NoFound/index.tsx
+++ time-evolution/src/Pages/NoFound/index.tsx
@@ -1,135 +1,22 @@
-import React, { ComponentProps } from 'react';
-import ReactDOM from 'react-dom'
+import React from 'react';
 
 import Header from '../../Components/Header/index';
 import Footer from '../../Components/Footer/index';
+import Game from './Components/Game';
 
 import './style.css';
-import { EndOfLineState } from 'typescript';
-
-interface ISquareProps {
-  value: number;
-  onClick: any;
-}
-
-interface ISquareState {
-  value?: string | null;
-}
-
-interface IBoardProps {
-}
-
-interface IBoardState {
-  squares: any;
-  xIsNext: boolean;
-}
-
-function calculateWinner(squares: any) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null;
-}
-
-function Square(props: ISquareProps) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component<IBoardProps, IBoardState> {
-  constructor(props: IBoardProps) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  handleClick(i: any) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) return;
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  renderSquare(i: number) {
-    return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
-    />;
-  }
-
-  render() {
-    let status;
-    const winner = calculateWinner(this.state.squares);
-
-    status = winner ? `Winner: ${winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="grid">
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default function NotFound() {
   return (
     <>
       <Header />
-      <Game />
+      <div className="main">
+        4
+        <div className="game">
+          <Game />
+        </div>
+        4
+      </div>
       <Footer />
     </>
   )
